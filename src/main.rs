aa63c1dfca4208b09410e31991fea5a9e9c502e0
Cameron King <gabrilend@gmail.com>
2021-01-25 14:46:00 -0800

    Hello specific people! With plugin implementation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
 use bevy::prelude::*;
 
-fn main() {
-    App::build()
-    .add_system(hello_world.system())
-    .run();
-}
+struct Person;
+struct Name(String);
+
+pub struct HelloPlugin;
 
 fn hello_world() {
     println!("hello world!");
+}
+
+
+// Creates a
+fn add_people(commands: &mut Commands) {
+    commands
+        .spawn((Person, Name("Elaina Proctor".to_string())))
+        .spawn((Person, Name("Renzo Hume".to_string())))
+        .spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_startup_system(add_people.system())
+            .add_system(hello_world.system())
+            .add_system(greet_people.system());
+    }
+}
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
 }
